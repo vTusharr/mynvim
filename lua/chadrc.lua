653e33e6007d682d58cbd4f422825e8491ea4508
Tushar <tusharverma8181@gmail.com>
2025-07-25 00:56:19 +0530

    wuiij

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -8,20 +8,12 @@ local M = {}
 M.base46 = {
 	theme = "catppuccin",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	 hl_override = {
+	 	Comment = { italic = true },
+	 	["@comment"] = { italic = true },
+	 },
 }
-M.options = {
-  -- This is the official recommended configuration from :h shell-powershell
-  shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-  shellcmdflag = "-NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering='plaintext';Remove-Alias -Force -ErrorAction SilentlyContinue tee;",
-  shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode',
-  shellpipe = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode',
-  shellquote = "",
-  shellxquote = "",
-}
+
 
 
  M.nvdash = { load_on_startup = true }
